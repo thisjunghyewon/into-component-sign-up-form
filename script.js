75e2a2553fa17b9e655a5e8b3008424aa59d80ea
thisjunghyewon <120668221+thisjunghyewon@users.noreply.github.com>
2023-03-22 18:39:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,68 @@
 const form = document.getElementById('form');
-const first = document.getElementById('first');
-const last = document.getElementById('last');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
 
-
-
-
-
-form.addEventListener('submit', e => {
+form.addEventListener ('submit', e=> {
   e.preventDefault();
-  checkInputs();
-});
 
+  const firstname = form['firstname'].value;
+  const lastname = form['lastname'].value;
+  const email = form['email'].value;
+  const password = form['password'].value;
 
-function checkInputs() {
-  const firstValue = first.value.trim();
-  const lastValue = last.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
- 
+  const placeholder1 = document.getElementById('firstname');
+  const placeholder2 = document.getElementById('lastname');
+  const placeholder3 = document.getElementById('email');
+  const placeholder4 = document.getElementById('password');
 
-  if(firstValue === '') {
-    setErrorFor(first, 'First Name cannot be empty');
-    
-  } else {
-    setSuccessFor(first);
+  if(firstname === '') {
+    addError('firstname', 'First Name cannot be empty')
+    placeholder1.setAttribute('placeholder','');
+  }else {
+    removeError ('firstname')
   }
 
-  if(lastValue === '') {
-    setErrorFor(last, 'Last Name cannot be empty');
-    
-  } else {
-    setSuccessFor(last);
+  if(lastname === '') {
+    addError('lastname', 'Last Name cannot be empty')
+    placeholder2.setAttribute('placeholder','');
+  }else {
+    removeError ('Lastname')
   }
 
-  if(emailValue === '') {
-    setErrorFor(email, 'Email Address cannot be empty');
-    
-  } else if(!isEmail(emailValue)) {
-      setErrorFor(email, 'Looks like this is not an email');
-  } else {
-    setSuccessFor(email);
+  if(email === '') {
+    addError('email', 'Email Address cannot be empty')
+    placeholder3.setAttribute('placeholder','email@example/com','red');
+  }else if(!isValid(email)) {
+    addError('email', 'Looks like this is not an email')
+  }else {
+    removeError ('email')
   }
 
-  if(passwordValue === '') {
-    setErrorFor(password, 'Password cannot be empty');
-    
-  } else {
-    setSuccessFor(password);
+  if(password === '') {
+    addError('password', 'Password cannot be empty')
+    placeholder4.setAttribute('placeholder','');
+  }else {
+    removeError ('password') 
   }
+  
+})
+
+function addError(field, message) {
+  const formControl = form[field].parentNode;
+  formControl.classList.add('error');
+  const small = form[field].parentNode.querySelector('small');
+  small.innerText = message;
 }
 
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector('small');
-  // add error message inside small
-  small.innerText = message;
-  formControl.className = 'form-control error';
+function removeError(field) {
+  const formControl = form[field].parentNode;
+  formControl.classList.remove('error');
+}
+
+function isValid(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 function setSuccessFor(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
-}
-
-function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
